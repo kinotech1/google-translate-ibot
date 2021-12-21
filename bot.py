774b87b1895d55df77785c64d35252e22774d889
kinotech1 <96439913+kinotech1@users.noreply.github.com>
2021-12-21 12:47:07 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ def start(updater,context):
 def echo(updater,context):
  usr_msg =updater.message.text
  translator = Translator()
- translation = translator.translate(usr_msg,dest='hi') 
+ translation = translator.translate(usr_msg,dest='ru') 
  updater.message.reply_text(translation.text)
  
 dp =updater.dispatcher.add_handler
