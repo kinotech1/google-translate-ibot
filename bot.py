cd7105135da7d5b0f780b55b65a9f73c1ed6fe93
kinotech1 <96439913+kinotech1@users.noreply.github.com>
2021-12-21 12:42:17 +0500

    bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+from telegram.ext import Updater,MessageHandler,CommandHandler,Filters
+from googletrans import Translator
+
+updater = Updater('5002975033:AAHOGF609JtXoNdlpcL6Ob4v83c_xmw3jJQ',use_context = True )
+
+def start(updater,context):
+ updater.message.reply_text('hi iam google translater ')
+ 
+def echo(updater,context):
+ usr_msg =updater.message.text
+ translator = Translator()
+ translation = translator.translate(usr_msg,dest='hi') 
+ updater.message.reply_text(translation.text)
+ 
+dp =updater.dispatcher.add_handler
+dp(CommandHandler('start',start))
+dp(MessageHandler(Filters.text,echo))
+
+updater.start_polling()
+updater.idle()
